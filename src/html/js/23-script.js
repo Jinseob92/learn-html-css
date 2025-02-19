d0ecefabad9f2851295a004737e874979153ad84
jinseob92 <elso1234@naver.com>
2025-02-19 14:18:32 +0900

    form select dialog details script styles

---
+++ src/html/js/23-script.js
@@ -0,0 +1,14 @@
+document.querySelector('.flip-button').addEventListener('click', () => {
+  document.querySelector('.card').classList.toggle('is-flipped');
+});
+
+// <script type="module">
+//       document.addEventListener('DOMContentLoaded', () => {
+//         const flipButton = document.querySelector('.flip-button');
+//         const card = document.querySelector('.card');
+
+//         flipButton.addEventListener('click', () => {
+//           card.classList.toggle('is-flipped');
+//         });
+//       });
+//     </script>
